759388d0163ffb287a5e60b3d1a2e9bce1f878aa
PeytonRod <89867794+PeytonRod@users.noreply.github.com>
2023-09-14 13:16:12 -0500

    Mouse Movement 1
    Added simple mouse movements w/ temp cursor

---
+++ Main Project/Assets/Sprites/Scripts/MouseController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePositionInWorld.z = 0;
+        transform.position = mousePositionInWorld;
+    }
+}
