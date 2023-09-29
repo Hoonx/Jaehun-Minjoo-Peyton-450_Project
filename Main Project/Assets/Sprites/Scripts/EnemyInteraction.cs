2b94cdc65752a503be461fe1f689e625ccfd4a10
Hoonx <106128890+Hoonx@users.noreply.github.com>
2023-09-29 12:55:10 -0500

    Destroy game object if health reaches 0.

--- Main Project/Assets/Sprites/Scripts/EnemyInteraction.cs
+++ Main Project/Assets/Sprites/Scripts/EnemyInteraction.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class EnemyInteraction : MonoBehaviour
 {
+    // Enemy Health
+    private float health;
+    // How Much Money the Enemy Drops
+    private int reward;
+    // How Much Damage the Enemy Does if it Reaches the End
+    private int damage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Call enemy Setup
     }
 
+    private void enemySetup()
+    {
+        // We should implement the movement here and below with additional functions.
+    }
+
+    public void takeDamage(float x)
+    {
+        health -= x;
+        if (health <= 0) {
+            death();
+        }
+    }
+
+    private void death() 
+    {
+        Destroy(transform.gameObject);
+    }
     // Update is called once per frame
     void Update()
     {
