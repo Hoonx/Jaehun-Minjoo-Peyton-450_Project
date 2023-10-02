cd351c2465c50691fc2ea4b5422b4ca0e863f1df
PeytonRod <89867794+PeytonRod@users.noreply.github.com>
2023-10-02 13:11:39 -0500

    Fixed Tower controls

--- Main Project/Assets/Sprites/Scripts/PlatformController.cs
+++ Main Project/Assets/Sprites/Scripts/PlatformController.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class platformController : MonoBehaviour
+public class PlatformController : MonoBehaviour
 {
     public int orientation;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
