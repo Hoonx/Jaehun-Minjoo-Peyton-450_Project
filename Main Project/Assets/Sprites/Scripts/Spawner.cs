81878fb95c50a8ff86987f6ce66bd1310b11893a
minjoopark <54569234+minjoopark@users.noreply.github.com>
2023-10-06 16:53:48 -0500

    bug fixes and enemies spawn wip

--- Main Project/Assets/Sprites/Scripts/Spawner.cs
+++ Main Project/Assets/Sprites/Scripts/Spawner.cs
@@ -4,25 +4,47 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    private GameObject[] Zombie;
+    public GameObject[] Zombie;
     private int wave = 1;
     private int enemiesNum;
     private float spawnTime;
+    private bool isSpawn = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemiesNum = wave * (enemiesNum + 5);
+        enemiesNum = enemiesForWave(wave);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnTime += Time.deltaTime;
+        //spawnTime += Time.deltaTime;
+        //spawnTime = 0f;
+        
+        if (isSpawn == true && enemiesNum>0)
+        {
+            Spawn();
+            enemiesNum--;
+            wave++;
 
-        spawnTime = 0f;
-        enemiesNum--;
-        wave++;
+        }
     }
+
+    private void Spawn()
+    {
+        if (Zombie.Length > 0)
+        {
+            GameObject spawnedZombies = Zombie[0];
+            Instantiate(spawnedZombies, LevelManager.main.startPoint.position, Quaternion.identity);
+        }
+    }
+
+    private int enemiesForWave(int wave)
+    {
+        return wave * 5; // Adjust this formula as needed for your game
+    }
+
+
 }
