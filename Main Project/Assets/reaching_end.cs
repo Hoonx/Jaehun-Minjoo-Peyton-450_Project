eb2cd1469f0a02fca6dffaad1ca4d186a74ac2c3
PeytonRod <89867794+PeytonRod@users.noreply.github.com>
2023-10-06 23:41:02 -0500

    Added restart button

--- Main Project/Assets/reaching_end.cs
+++ Main Project/Assets/reaching_end.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NewWave;
+using UnityEngine.SceneManagement;
 
 public class reaching_end : MonoBehaviour
 {
     public int health = 3;
+    public GameObject restartButton;
+    private void Start()
+    {
+        restartButton.SetActive(false);
+    }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "enemy") {
             Debug.Log("End Reached");
             health -= 1;
+            
         }
     }
-
-    void update {
+   
+    void Update() {
         if (health <= 0) {
             // Reset Level
-            spawner.StartWave;
+            Time.timeScale = 0;
+            restartButton.SetActive(true);
+            
         }
     }
 }
