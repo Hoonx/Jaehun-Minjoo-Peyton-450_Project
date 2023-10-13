3cdb845b671a7772c98030a6f98941b56e3c27ca
minjoopark <54569234+minjoopark@users.noreply.github.com>
2023-10-13 18:25:19 -0500

    currency display and buybutton update

--- Main Project/Assets/Sprites/Scripts/BuyButton.cs
+++ Main Project/Assets/Sprites/Scripts/BuyButton.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuyButton : MonoBehaviour
 {
     // Start is called before the first frame update
     public int money;
-    public int unitsLeft;
+    //public int unitsLeft;
     public GameObject unit;
+    public Text moneyText;
 
+    //public void Start()
+    //{
+    //    money = 500;
+    //    UpdateMoneyDisplay();
+    //}
     public void OnButtonPress() {
-        if (money >= 200 && unitsLeft > 0) {
+        if (money >= 200) {
             money -= 200;
-            unitsLeft--;
+            //unitsLeft--;
+            UpdateMoneyDisplay();
             GameObject newUnit = Instantiate(unit);
             newUnit.transform.position = new Vector3(0, 0, 0);
 
         }
 
+    }
+    private void UpdateMoneyDisplay()
+    {
+        moneyText.text = "Money: " + money.ToString();
     }
 
 }
