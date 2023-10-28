d0a74cf5cbeaf7283d5c0312f730df3deb2713a9
Hoonx <106128890+Hoonx@users.noreply.github.com>
2023-10-28 02:38:12 -0500

    Projectile Fire (Not Done)

---
+++ Main Project/Assets/J_Projectile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_Projectile : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    private Transform target;
+    [SerializeField] private float speed = 5f;
+
+    private Transform target;
+
+    public void defineTarget(Transform _target) {
+        target = _target;
+    }
+
+    private void FixedUpdate() {
+        if (!target) {
+            return;
+        }
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll) {
+        Destroy(gameObject);
+    }
+}
--- Main Project/Assets/J_Turret.cs
+++ Main Project/Assets/J_Turret.cs
@@ -5,20 +5,20 @@ using UnityEditor;
 
 public class J_Turret : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private Transform turretRotationPoint;
-    [Header("Attribute")]
+    [SerializeField] private Transform rotationPoint;
     [SerializeField] private float range = 5f;
     [SerializeField] private float rotationSpeed = 200;
     [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float dps = 2f;
+
     private Transform target;
+    private float waitTime;
 
     private void OnDrawGizmosSelected() {
         Handles.color = Color.red;
         Handles.DrawWireDisc(transform.position, transform.forward, range);
-        {
-            
-        }
     }
 
     private void Update() {
@@ -27,16 +27,32 @@ public class J_Turret : MonoBehaviour
             return;
         }
         RotateTowardTarget();
+        if (!isTargetInRange()) {
+            target = null;
+        }
+        else {
+            waitTime += Time.deltaTime;
+            if (waitTime >= 1f/dps) {
+                fire();
+                waitTime = 0f;
+            }
+        }
     }
 
-    private void isTargetInRange() {
+    private void fire() {
+        GameObject projectileObject = Instantiate(projectile, firePoint.position, Quaternion.identity);
+        Projectile projectileScript = projectileObject.GetComponent<Projectile>();
+        projectileScript.defineTarget(target);
+    }
 
+    private bool isTargetInRange() {
+        return Vector2.Distance(target.position, transform.position) <= range;
     }
 
     private void RotateTowardTarget() {
         float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        rotationPoint.rotation = Quaternion.RotateTowards(rotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
     private void FindTarget() {
