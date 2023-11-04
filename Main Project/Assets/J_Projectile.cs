925e5b7a1c37f0297c60c5ec00db4bd0df6374fe
Hoonx <106128890+Hoonx@users.noreply.github.com>
2023-11-04 18:47:36 -0500

    Working On Projectile

--- Main Project/Assets/J_Projectile.cs
+++ Main Project/Assets/J_Projectile.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class J_Projectile : MonoBehaviour
 {
-    // [SerializeField] private Rigidbody2D rb;
-    // private Transform target;
-    // [SerializeField] private float speed = 5f;
+    [SerializeField] private Rigidbody2D rb;
+    private Transform target;
+    [SerializeField] private float speed = 5f;
 
-    // private Transform target;
+    public void defineTarget(Transform _target) {
+        target = _target;
+    }
 
-    // public void defineTarget(Transform _target) {
-    //     target = _target;
-    // }
+    private void FixedUpdate() {
+        if (!target) {
+            return;
+        }
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * speed;
+    }
 
-    // private void FixedUpdate() {
-    //     if (!target) {
-    //         return;
-    //     }
-    //     Vector2 direction = (target.position - transform.position).normalized;
-    //     rb.velocity = direction * speed;
-    // }
-
-    // private void OnCollisionEnter2D(Collision2D coll) {
-    //     Destroy(gameObject);
-    // }
+    private void OnCollisionEnter2D(Collision2D coll) {
+        Destroy(gameObject);
+    }
 }
